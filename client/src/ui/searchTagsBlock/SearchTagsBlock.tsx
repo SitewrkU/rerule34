821564dfd6521c73a93e3793de63f03d79b866da
Sitewku <Sitesentsii@gmail.com>
2026-07-20 20:41:20 +0300

    search tags block: new styles

--- client/src/ui/searchTagsBlock/SearchTagsBlock.tsx
+++ client/src/ui/searchTagsBlock/SearchTagsBlock.tsx
@@ -1,7 +1,8 @@
-import {ChevronDown, Tag, RemoveSquare, AddSquare} from "clicons-react";
+import {ChevronDown, Tag, RemoveCircle, AddCircle} from "clicons-react";
 import { Button, Dropdown, Collapse } from 'antd';
 import styles from './SearchTagsBlock.module.css'
 import {blackThemes, useBlacklistStore} from "../../store/blackListStore.ts";
+import clsx from "clsx";
 
 const SearchTagsBlock = () => {
   const selected = useBlacklistStore((s) => s.selected)
@@ -18,7 +19,17 @@ const SearchTagsBlock = () => {
       trigger={['hover']}
       popupRender={() => (
         <div className={styles.dropdown}>
-          <p className={styles.ddHeader}><Tag size={18} strokeWidth={3}/> Додаткові теги, які відкидаються або додаються під час пошуку</p>
+
+          <div className={styles.ddHeader}>
+            <div className={styles.headerIcon}>
+              <Tag size={18} strokeWidth={3}/>
+            </div>
+            <div className={styles.headerText}>
+              <p>Додаткові теги</p>
+              <p>Відкидаються або додаються під час пошуку</p>
+            </div>
+          </div>
+
           <Collapse
             className={styles.collapse}
             ghost
@@ -27,18 +38,27 @@ const SearchTagsBlock = () => {
               {
                 key: '1',
                 label: (
-                  <p><RemoveSquare className={styles.iconRemove}/> Теги, які були прибрані з пошуку:</p>
+                  <div className={clsx(styles.collapseHeader, styles.chRemove)}>
+                    <RemoveCircle/>
+                    <p>Теги, які виключаються</p>
+                    <p>-{filteredTags.length}</p>
+                  </div>
+
                 ),
                 children: <p className={styles.tagstoremove}>
                   {filteredTags.map((tag, i) => (
-                    <span key={i}>{tag} </span>
+                    <span key={i}>{tag}</span>
                   ))}
                 </p>,
               },
               {
                 key: '2',
                 label: (
-                  <p className={styles.tagstoadd}><AddSquare className={styles.iconAdd}/> Теги, які були добавлені в пошук:</p>
+                  <div className={clsx(styles.collapseHeader, styles.chAdd)}>
+                    <AddCircle/>
+                    <p>Теги, які додаються</p>
+                    <p>+0</p>
+                  </div>
                 ),
                 children: <p>Тут будуть теги</p>,
               },
@@ -48,8 +68,9 @@ const SearchTagsBlock = () => {
         </div>
       )}
     >
-      <Button className={styles.button} icon={<ChevronDown />}>
+      <Button className={styles.button} icon={<ChevronDown className={styles.dropIcon}/>}>
         <div>
+          <Tag size={18} strokeWidth={3} className={styles.tagIcon}/>
           <p className={styles.removed}>-{filteredTags.length}</p>
           <p className={styles.added}>+0</p>
         </div>
